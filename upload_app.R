3f4eb14de4bb18326782c47ed98627d2c79c8a97
cimentadaj <cimentadaj@gmail.com>
2016-12-22 18:22:11 +0100

    Updated directory in shiny deploy

--- upload_app.R
+++ upload_app.R
@@ -5,4 +5,9 @@ rsconnect::setAccountInfo(name='cimentadaj',
                           token='B599AA7AE9508743A906E74E7D03893F',
                           secret='VIP6Yxhbczog4B53jkQve2hpTqsTHFFkPDqa/LP/')
 
-rsconnect::deployApp('/Users/cimentadaj/Downloads/inequality/shiny')
+rsconnect::deployApp('/Users/cimentadaj/Downloads/inequality/Inequality_Shinyapp/shiny/')
+
+cd = 'cd /Users/cimentadaj/Downloads/inequality/Inequality_Shinyapp/'
+add = 'git add .'
+commit = 'git commit'
+push = 'git push'
